15b5a1a1ea45fa4f5daecad7e3a975dbdf839928
YL <simon-ling@hotmail.com>
2021-09-23 19:52:00 -0400

    done

--- src/HelloWorld.scala
+++ src/HelloWorld.scala
@@ -1,4 +1,3 @@
-
 //new version
 //object HelloWorld extends App {
 //  println("Hello World")
@@ -7,15 +6,7 @@
 //old version
 object HelloWorld {
   def main(args: Array[String]): Unit = {
-    //s string interpolation | f string interpolation
-    val name="mark"
-    val age=18
-    val height=32.12
-    println(s"name: $name, age: $age, height: $height")
-    println(f"name: $name%s, age: $age%d, height: $height%f")
-    println(s"Hello \nWorld");
-    println(raw"Hello \nWorld"); 
-    
+    println("Hello World")
   }
   
 }
---
+++ src/StringInterpolation.scala
@@ -0,0 +1,14 @@
+
+
+object StringInterpolation {
+  def main(args: Array[String]): Unit = {
+    //s string interpolation | f string interpolation
+    val name="mark"
+    val age=18
+    val height=32.12
+    println(s"name: $name, age: $age, height: $height")
+    println(f"name: $name%s, age: $age%d, height: $height%f")
+    println(s"Hello \nWorld");
+    println(raw"Hello \nWorld"); 
+  }
+}
