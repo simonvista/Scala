57cf3616ebe5e699ee7648d80cb364582dc2603d
YL <simon-ling@hotmail.com>
2021-09-23 18:38:32 -0400

    done

---
+++ src/HelloWorld.scala
@@ -0,0 +1,13 @@
+
+//new version
+//object HelloWorld extends App {
+//  println("Hello World")
+//}
+
+//old version
+object HelloWorld {
+  def main(args: Array[String]): Unit = {
+    println("Hello World")
+  }
+  
+}
