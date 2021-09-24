033189975dcf20ed0720f8b2dd25f9a4171565d6
YL <simon-ling@hotmail.com>
2021-09-23 21:45:57 -0400

    done

--- src/Function.scala
+++ src/Function.scala
@@ -5,13 +5,13 @@ object Function {
    * @author YL
    */
   object Math {
-    def addTwo(x:Int,y:Int)=x+y
-    def subtractTwo(x:Int,y:Int)=x-y
-    def multiplyTwo(x:Int,y:Int)=x*y
-    def divideTwo(x:Int,y:Int)=x/y
-    def square(x:Int)=x*x
+    def addTwo(x: Int, y: Int) = x + y
+    def subtractTwo(x: Int, y: Int) = x - y
+    def multiplyTwo(x: Int, y: Int) = x * y
+    def divideTwo(x: Int, y: Int) = x / y
+    def square(x: Int) = x * x
   }
-  
+
   def main(args: Array[String]): Unit = {
     println(Math.addTwo(11, -22))
     println(Math.subtractTwo(11, -32))
