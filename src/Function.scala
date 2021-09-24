58a53644907ee2fc8120051fecc7cc73a9861b54
YL <simon-ling@hotmail.com>
2021-09-23 21:20:41 -0400

    done

---
+++ src/Function.scala
@@ -0,0 +1,23 @@
+
+
+object Function {
+  /**
+   * @author YL
+   */
+  object Math {
+    def addTwo(x:Int,y:Int)=x+y
+    def subtractTwo(x:Int,y:Int)=x-y
+    def multiplyTwo(x:Int,y:Int)=x*y
+    def divideTwo(x:Int,y:Int)=x/y
+    def square(x:Int)=x*x
+  }
+  
+  def main(args: Array[String]): Unit = {
+    println(Math.addTwo(11, -22))
+    println(Math.subtractTwo(11, -32))
+    println(Math.multiplyTwo(-11, -11))
+    println(Math.divideTwo(21, 9))
+    //println(Math square -3) b/c square has only one argument
+    println(Math.square(-3))
+  }
+}
